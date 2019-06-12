e91967734962160968b1876692dd7851a61b9965
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2019-06-12 22:03:30 +0800

    no message

--- UgandaScrape/UgandaDemo.py
+++ UgandaScrape/UgandaDemo.py
@@ -4,6 +4,8 @@ import bs4
 import future.backports.urllib.request
 import pandas.io.common
 import pandas as pd
+import pymysql
+import pyodbc
 
 
 def get_bs_obj(link):
@@ -28,18 +30,25 @@ def get_date(bs_obj):
     date = content_date.strip().split(' ')[-1]
     date_dt2 = datetime.strptime(date, '%d-%b-%Y')
     print(date_dt2)
-    print(date_dt2.strftime('%Y%m%d'))
-    return date_dt2.strftime('%Y%m%d')
+    print(date_dt2.strftime('%Y-%m-%d'))
+    return date_dt2.strftime('%Y-%m-%d')
 
-def print_details(country_name, link, id_macro_ent):
-    country = Country_stock_index(country_name, link, id_macro_ent)
-    print(country.country_name, country.code, country.date, country.value, country.link, country.id)
-    list_country_name.append(country.country_name)
-    list_country_code.append(country.code)
-    list_country_date.append(country.date)
-    list_country_value.append(country.value)
-    list_country_link.append(country.link)
-    list_country_id.append(country.id)
+def create_query(macro_id, source, date, value):
+    query = 'EXECUTE TIER2.PROD.SP_INSERT_RATE ' + str(macro_id) + ', ' + str(source) + ', \'' + str(date) + '\', ' + str(value)
+    print(query)
+    return query
+
+def print_details(country_name, link, id_macro_ent, source):
+    country = Country_stock_index(country_name, link, id_macro_ent, source)
+    print(country.country_name, country.code, country.date, country.value, country.link, country.id, country.source)
+    list_query.append(create_query(country.id, country.source, country.date, country.value))
+    list_name.append(country.country_name)
+    list_code.append(country.code)
+    list_date.append(country.date)
+    list_value.append(country.value)
+    list_link.append(country.link)
+    list_id.append(country.id)
+    list_source.append(country.source)
 
 def get_details(data_frame):
     name_list = data_frame['Country'].values
@@ -47,13 +56,34 @@ def get_details(data_frame):
         country = data_frame['Country'] == name
         website = (data_frame[country])['Website'].values[0]
         id_macro_ent = (data_frame[country])['ID_macro_ent'].values[0]
-        print_details(name, website, id_macro_ent)
+        source = (data_frame[country])['Source'].values[0]
+        print_details(name, website, id_macro_ent, source)
 
+def connect_mysql_server(result):
+    file = pd.read_excel(result)
+    connection = pymysql.connect(host='localhost', user='LinhChi', password='139/6/4_Chi', db='criweb_admin')
+    cursor = connection.cursor()
+    sql = 'SELECT * FROM `django_content_type`;'
+    cursor.execute(sql)
+    count_row = cursor.execute(sql)
+    print("Number of rows: ", count_row)
+    # fetchone: show only one result - the first line
+    # fetchall: show all the results
+    # fetchmany: show the specific number of results
+    data_one = cursor.fetchone()
+    data_all = cursor.fetchall()
+    for data in data_all:
+        print(data)
+        for i in data:
+            print(i)
+    # print(data_one)
+    # print(data_all)
 
 class Country_stock_index(object):
-    def __init__(self, country_name, link, id_macro_ent):
+    def __init__(self, country_name, link, id_macro_ent, source):
         self.country_name = country_name
         self.id = id_macro_ent
+        self.source = source
         bs_obj = get_bs_obj(link)
         self.code = get_code(bs_obj)
         self.date = get_date(bs_obj)
@@ -63,16 +93,14 @@ class Country_stock_index(object):
     def get_country_name(self):
         return self.country_name
 
-list_country_name = list()
-list_country_code = list()
-list_country_date = list()
-list_country_value = list()
-list_country_link = list()
-list_country_id = list()
-file = pandas.read_excel('MacroData_WebsiteSources - Copy.xlsx')
-df = pandas.DataFrame(file, columns=['ID_macro_ent', 'Country', 'Source', 'Website'])
-get_details(df)
-
-zipped = list(zip(list_country_id, list_country_name, list_country_code, list_country_value, list_country_date, list_country_link))
-df = pd.DataFrame(zipped, columns=['ID_macro_ent', 'Country Name', 'Index Code', 'Value', 'Date', 'Link'])
-df.to_excel('file_clean.xlsx', index=False)
+list_name = list()
+list_code = list()
+list_date = list()
+list_value = list()
+list_link = list()
+list_id = list()
+list_source = list()
+list_query = list()
+file_name = 'MacroData_WebsiteSources - Copy.xlsx'
+file = pandas.read_excel(file_name)
+connect_mysql_server(file_name)
