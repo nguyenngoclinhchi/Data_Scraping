7ef22b703dda5e028b4c96bc3413d101b451460a
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2019-06-13 09:47:02 +0800

    no message

--- UgandaScrape/run.bat
+++ UgandaScrape/run.bat
@@ -1,13 +1,11 @@
 :: show the version of Window version ----------------------------------------------------------------------------------
 @echo off
 ver
-
 :: This is a batch command that associates an extension with a file type (FTYPE), displays existing associations,
 :: or deletes an association -------------------------------------------------------------------------------------------
 :: @echo off
 :: assoc > D:\Kitty\NUS\Internship-PartTimeJob\RMI-CRI\Uganda-Scraping\UgandaScrape\lists.txt
 :: assoc | find “.py” > D:\Kitty\NUS\Internship-PartTimeJob\RMI-CRI\Uganda-Scraping\UgandaScrape\listsdoc.txt
-
 :: The following example shows how the cd command can be used in a variety of ways. ------------------------------------
 Rem The cd without any parameters is used to display the current working directory
 @echo off
@@ -27,12 +25,10 @@ cd
 Rem Changing back to the D:\Kitty\NUS\Internship-PartTimeJob\RMI-CRI\Uganda-Scraping\UgandaScrape
 cd /d D:\Kitty\NUS\Internship-PartTimeJob\RMI-CRI\Uganda-Scraping\UgandaScrape
 cd
-
 :: This batch command run the UgandaDemo.py ----------------------------------------------------------------------------
 @echo off
 python UgandaDemo.py
-
-:: Copy the files
+:: Copy the files-------------------------------------------------------------------------------------------------------
 @echo off
 cd
 Rem Copies lists.txt to the present working directory. If there is no
